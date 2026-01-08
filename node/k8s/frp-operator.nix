586cc3f13c2c66245a11b367ed711cb5104ab4d4
Артем Старченко <41ways1ucky@gmail.com>
2026-01-09 00:05:41 +0300

    fix

--- node/k8s/frp-operator.nix
+++ node/k8s/frp-operator.nix
@@ -7,7 +7,7 @@ in
     name = "frp-operator";
     repo = "https://zufardhiyaulhaq.com/frp-operator/charts/releases/";
     version = "1.4.0";
-    hash = "sha256-J9t2HFrSUl/RMMkv4vLUUA+IcOQC/v48nLjTTYpxpww=";
+    hash = "sha256-A2GBAl2G6HHjbMlFsYAMbwHs4JYY3CMhofg/Eoc0TAQ=";
     targetNamespace = namespace;
     createNamespace = true;
   };
