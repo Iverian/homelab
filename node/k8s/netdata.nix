a5b5d97218bf6ae7e31e5a8cdc388cb1ef988c3b
Артем Старченко <41ways1ucky@gmail.com>
2026-01-10 13:51:07 +0300

    fix

--- node/k8s/netdata.nix
+++ node/k8s/netdata.nix
@@ -16,4 +16,43 @@ in
       k8sState.env.DO_NOT_TRACK = 1;
     };
   };
+  services.k3s.manifests.netdata-httproute.content = {
+    apiVersion = "gateway.networking.k8s.io/v1";
+    kind = "HTTPRoute";
+    metadata = {
+      name = "netdata";
+      namespace = namespace;
+    };
+    spec = {
+      hostnames = [ "netdata.home.iverian.ru" ];
+      parentRefs = [
+        {
+          group = "gateway.networking.k8s.io";
+          kind = "Gateway";
+          name = "main";
+          namespace = "envoy-gateway-system";
+        }
+      ];
+      rules = [
+        {
+          backendRefs = [
+            {
+              group = "";
+              kind = "Service";
+              name = "netdata";
+              port = 19999;
+            }
+          ];
+          matches = [
+            {
+              path = {
+                type = "PathPrefix";
+                value = "/";
+              };
+            }
+          ];
+        }
+      ];
+    };
+  };
 }
